d9ac10298907088534ddb518f2368a76cce9cd60
Reiya Matsumoto <ce235037@meiji.ac.jp>
2023-08-22 15:34:32 +0900

    #11 ログインページの作成

---
+++ client/src/pages/History.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from "vue";
+
+const message = ref("hello world!");
+</script>
+
+<template>
+    <div>history</div>
+    <h1>{{ message }}</h1>
+</template>
+<style>
+</style>
