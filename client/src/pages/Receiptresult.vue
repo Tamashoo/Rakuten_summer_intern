9db182baf1505bc112535a3fc7c7518581694095
nkz5 <kantoodeeen@gmail.com>
2023-08-22 17:00:04 +0900

    #23 receiptresultのページを作成

--- client/src/pages/Receiptresult.vue
+++ client/src/pages/Receiptresult.vue
@@ -1,8 +1,26 @@
 <script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
+
+const Router = useRouter();
+
+const result = ref(10);
 
 </script>
 <template>
-    <div>receiptresult</div>
+    <h1>送信完了</h1>
+    <h2>獲得Exp:</h2>
+    <div class="Result">{{ result }}</div>
+    <button @click="Router.push('/home')" class="HomeBtn">ホーム画面に戻る</button>
 </template>
 <style>
+.Result{
+    text-align: center;
+    font-size: 100px;
+}
+.HomeBtn{
+    position: absolute;
+    bottom: 20px;
+}
 </style>
