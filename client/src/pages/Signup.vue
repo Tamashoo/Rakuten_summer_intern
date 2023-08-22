1e7bf5ead90bf839c8fe92d20ff19dd77b34ad4a
nkz5 <kantoodeeen@gmail.com>
2023-08-22 13:53:44 +0900

    #12 make signup form

--- client/src/pages/Signup.vue
+++ client/src/pages/Signup.vue
@@ -1,8 +1,35 @@
 <script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const Router = useRouter();
+
+const Name = ref('');
+const Password = ref('');
+
+const register = () => {
+    //登録する処理
+
+    Router.push('/home');
+}
 
 </script>
 <template>
-    <div>signup</div>
+    <h1 class="aiueo">サインアップ</h1>
+    <form @submit="register">
+        <p>名前</p>
+        <input required class="input" v-model="Name" placeholder="名前" />
+        <p>パスワード</p>
+        <input required class="input" v-model="Password" placeholder="パスワード" />
+        <p />
+        <button class="button">登録</button>
+    </form>
 </template>
 <style>
+.aiueo{
+    text-align: center;
+}
+.button{
+    color: blue;
+}
 </style>
