1b36422b086dd185475496dee7d2682f9a6b28f4
nkz5 <kantoodeeen@gmail.com>
2023-08-23 16:44:53 +0900

    #47 レシート送信結果画面のデザイン

--- client/src/pages/Receiptresult.vue
+++ client/src/pages/Receiptresult.vue
@@ -22,8 +22,11 @@ onMounted(() => {
 </script>
 <template>
     <h1>送信完了</h1>
-    <h2>獲得Exp:</h2>
-    <div class="Result">{{ result }}</div>
+    
+    <div class="rounded-3xl w-80 h-80 border-4 border-rakuten text-center mx-auto mt-6 bg-amber-100">
+        <h2 class="font-bold text-2xl text-rakuten">獲得Exp</h2>
+    <div class="text-9xl text-rakuten font-black m-10">{{ result }}</div>
+    </div>
     <button @click="Router.push('/home')" class="HomeBtn">ホーム画面に戻る</button>
 </template>
 <style>
