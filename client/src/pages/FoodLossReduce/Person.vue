45f684c1466d60d7187809911f5aeb527df4d308
nkz5 <kantoodeeen@gmail.com>
2023-08-23 10:07:03 +0900

    #24 FoodLossReduce Person page

--- client/src/pages/FoodLossReduce/Person.vue
+++ client/src/pages/FoodLossReduce/Person.vue
@@ -1,8 +1,35 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
+
+const Router = useRouter();
+
+const Contribute = ref(1234);
+
+onMounted(() => {
+    axios.get("api/hoodlossreduce/person")
+    .then(response => {
+        Contribute.value = response;
+    })
+    .catch(error => {
+        console.log(error);
+    })
+});
 
 </script>
 <template>
-    <div>FLRperson</div>
+    <h1>あなたのフードロス削減度</h1>
+    <div class="contribute">{{ Contribute }}</div>
+    <button @click="Router.push('/home')" class="HomeBtn">ホーム画面に戻る</button>
 </template>
 <style>
+.contribute{
+    text-align: center;
+    font-size: 100px;
+}
+.HomeBtn{
+    position: absolute;
+    bottom: 20px;
+}
 </style>
